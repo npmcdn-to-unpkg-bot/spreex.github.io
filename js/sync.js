c59312fb0a13c272fe8e5d1ed61f49a522b812f1
Denis Gukov <fiftin@outlook.com>
2016-06-14 03:19:08 +0600

    test: sync#getPostToChange

--- js/sync.js
+++ js/sync.js
@@ -1,138 +1,147 @@
-const ROOT = 'spreex.github.io';
-const FIELDS = [
-  'title',
-  'description',
-  'websiteURL',
-  'demoURL',
-  'sourceCodeURL',
-  'githubRepoName',
-  'rubygemsGemName'
-];
-const GITHUB_FIELDS = {
-  forks: 'forks',
-  subscribers_count: 'watchers',
-  open_issues: 'issues',
-  watchers: 'stars'
-}
+Sync = {
+  ROOT: 'spreex.github.io',
+  FIELDS: [
+    'title',
+    'description',
+    'websiteURL',
+    'demoURL',
+    'sourceCodeURL',
+    'githubRepoName',
+    'rubygemsGemName'
+  ],
+  GITHUB_FIELDS: {
+    forks: 'forks',
+    subscribers_count: 'watchers',
+    open_issues: 'issues',
+    watchers: 'stars'
+  },
+  /**
+   * Connect & login to MyDataSpace.
+   */
+  connectToStorage: function(done) {
+    Mydataspace.init({
+      connected: function() {
+        Mydataspace.on('login', function() {
+          done();
+        });
+      }
+    });
+    Mydataspace.connect();
+  },
 
-/**
- * Connect & login to MyDataSpace.
- */
-function connectToStorage(done) {
-  Mydataspace.init({
-    connected: function() {
-      Mydataspace.on('login', function() {
-        done();
-      });
+  getDataFromStorage: function(done) {
+    Mydataspace.request('entities.get', { root: Sync.ROOT, path: 'extensions' }, done);
+  },
+
+  getDataFromSite: function(done) {
+    $.ajax({
+      url: 'http://spreex.github.io/posts.js',
+      mathod: 'get',
+      dataType: 'json'
+    }).done(function(data) {
+      done(data);
+    });
+  },
+
+  getGithubRepo: function(postOnSite) {
+    const gh = new GitHub();
+    const parts = postOnSite.githubRepoName.split('/');
+    return gh.getRepo(parts[0], parts[1]).getDetails().then(function(details) {
+      const fields = [];
+      const data = details.data;
+      for (let field in Sync.GITHUB_FIELDS) {
+        field.push({
+          name: Sync.GITHUB_FIELDS[field],
+          value: data[field]
+        });
+      }
+      return {
+        root: Sync.ROOT,
+        path: 'extensions/' + postOnSite.name,
+        fields: fields
+      };
+    });
+  },
+
+  getPostsToRemove: function(postsOnSite, postsInStorage) {
+    return postsInStorage.filter(post => typeof common.findByName(postsOnSite, common.getChildName(post.path)) !== 'undefined');
+  },
+
+  isPostExistsInStorage: function(postInStorage, name) {
+    return !common.isNull(Sync.findPostInStorage(postInStorage, name));
+  },
+
+  findPostInStorage: function(postsInStorage, name) {
+    var ret = postsInStorage.filter(postInStorage => postInStorage.root === Sync.ROOT && postInStorage.path === 'extensions/' + name);
+    if (ret.length > 0) {
+      return ret[0];
     }
-  });
-  Mydataspace.connect();
-}
+    return null;
+  },
 
-function getDataFromStorage(done) {
-  Mydataspace.request('entities.get', { root: ROOT, path: 'extensions' }, done);
-}
-
-function getDataFromSite(done) {
-  $.ajax({
-    url: 'http://spreex.github.io/posts.js',
-    mathod: 'get',
-    dataType: 'json'
-  }).done(function(data) {
-    done(formatDataFromSite(data));
-  });
-}
-
-function getGithubRepo(postOnSite) {
-  const gh = new GitHub();
-  const parts = postOnSite.githubRepoName.split('/');
-  return gh.getRepo(parts[0], parts[1]).getDetails().then(function(details) {
-    const fields = [];
-    const data = details.data;
-    for (let field in GITHUB_FIELDS) {
-      field.push({
-        name: GITHUB_FIELDS[field],
-        value: data[field]
-      });
-    }
-    return {
-      root: ROOT,
-      path: 'extensions/' + postOnSite.name,
-      fields: fields
-    };
-  });
-}
-
-function getPostsToRemove(postsOnSite, postsInStorage) {
-  return postsInStorage.filter(post => typeof common.findByName(postsOnSite, common.getChildName(post.path)) !== 'undefined');
-}
-
-function isPostExistsInStorage(postInStorage, name) {
-  return !common.isNull(findPostInStorage(postInStorage, name));
-}
-
-function findPostInStorage(postInStorage, name) {
-  var ret = postsInStorage.filter(postInStorage => postInStorage.root === ROOT && postInStorage.path === 'extensions/' + name);
-  if (ret.length > 0) {
-    return ret[0];
-  }
-  return null;
-}
-
-function getPostsToCreate(postsOnSite, postsInStorage) {
-  var ret =
-    postsOnSite
-      .filter(postOnSite => !isPostExistsInStorage(postsInStorage, postOnSite.name))
-      .map(function(postOnSite) {
-        var res = { root: ROOT, path: 'extensions/' + postOnSite.name, fields: [] };
-        for (let field of FIELDS) {
-          res.fields.push({
-            name: field,
-            value: postOnSite[field]
-          });
-        }
-        return res;
-      });
-  return ret;
-}
-
-function getPostsToChange(postsOnSite, postsInStorage) {
-  var ret =
-    postsOnSite
-      .map(function(postOnSite) {
-        var postInStorage = findPostInStorage(postsInStorage, postOnSite.name);
-        if (common.isNull(postInStorage)) {
-          return null;
-        }
-        var res = { root: ROOT, path: 'extensions/' + postOnSite.name, fields: [] };
-        for (let field of FIELDS) {
-          if (postOnSite[field] === common.findByName(postInStorage.fields, field).value) {
-            continue;
+  getPostsToCreate: function(postsOnSite, postsInStorage) {
+    var ret =
+      postsOnSite
+        .filter(postOnSite => !Sync.isPostExistsInStorage(postsInStorage, postOnSite.name))
+        .map(function(postOnSite) {
+          var res = { root: Sync.ROOT, path: 'extensions/' + postOnSite.name, fields: [] };
+          for (let field of Sync.FIELDS) {
+            res.fields.push({
+              name: field,
+              value: postOnSite[field]
+            });
           }
-          res.fields.push({
-            name: field,
-            value: postOnSite[field]
-          });
-        }
-        return res;
-      });
-  return ret;
-}
+          return res;
+        });
+    return ret;
+  },
 
-function sync() {
-  connectToStorage(() => {
-    getDataFromSite((postsOnSite) => {
-      getDataFromStorage((postsInStorage) => {
-        Mydataspace.request('entities.delete', getPostsToRemove(postsOnSite, postsInStorage), function() {
-          Mydataspace.request('entities.create', getPostsToCreate(postsOnSite, postsInStorage), function() {
-            Mydataspace.request('entities.change', getPostsToChange(postsOnSite, postsInStorage), function() {
-              Promise.all(postsOnSite.map(postOnSite => getGithubRepo(postOnSite))).then(postsForUpdate => {
-                Mydataspace.emit('entities.change', postsForUpdate);
+  getPostsToChange: function(postsOnSite, postsInStorage) {
+    var ret =
+      postsOnSite
+        .map(function(postOnSite) {
+          var postInStorage = Sync.findPostInStorage(postsInStorage, postOnSite.name);
+          if (common.isNull(postInStorage)) {
+            return null;
+          }
+          var res = { root: Sync.ROOT, path: 'extensions/' + postOnSite.name, fields: [] };
+          for (let field of Sync.FIELDS) {
+            var fieldInStorage = common.findByName(postInStorage.fields, field);
+
+            if (typeof fieldInStorage !== 'undefined' && postOnSite[field] === fieldInStorage.value) {
+              continue;
+            }
+
+            if (typeof postOnSite[field] === 'undefined') {
+              continue;
+            }
+            
+            res.fields.push({
+              name: field,
+              value: postOnSite[field]
+            });
+          }
+          return res;
+        });
+    return ret;
+  },
+
+  sync: function() {
+    Sync.connectToStorage(() => {
+      Sync.getDataFromSite((postsOnSite) => {
+        Sync.getDataFromStorage((postsInStorage) => {
+          Mydataspace.request('entities.delete', Sync.getPostsToRemove(postsOnSite, postsInStorage), function() {
+            Mydataspace.request('entities.create', Sync.getPostsToCreate(postsOnSite, postsInStorage), function() {
+              Mydataspace.request('entities.change', Sync.getPostsToChange(postsOnSite, postsInStorage), function() {
+                Promise.all(postsOnSite.map(postOnSite => Sync.getGithubRepo(postOnSite))).then(postsForUpdate => {
+                  Mydataspace.emit('entities.change', postsForUpdate);
+                });
               });
             });
           });
         });
       });
     });
-  });
+  }
+
 }
--- test/syncSpec.js
+++ test/syncSpec.js
@@ -1,8 +1,25 @@
 'use strict';
 // var expect = require('chai').expect;
 
-describe('', function() {
-  it('', function() {
-    
+describe('getPostsToChange', function() {
+  it('returns posts for change', function() {
+    var res = Sync.getPostsToChange([
+      { name: 'spree_pages', title: 'spree_pages', description: 'This is a really basic page CMS' }
+    ], [
+      {
+        root: 'spreex.github.io',
+        path: 'extensions/spree_pages',
+        fields: [
+          { name: 'websiteURL', value: 'http://spreecommerce.com' },
+          { name: 'title', value: 'spree_pages' },
+          { name: 'description', value: 'Old description' },
+        ]
+      }
+    ]);
+    console.log(res[0].fields);
+    expect(res.length).to.eq(1);
+    expect(res[0].fields.length).to.eq(1);
+    expect(res[0].fields[0].name).to.eq('description');
+    expect(res[0].fields[0].value).to.eq('This is a really basic page CMS');
   });
 });
