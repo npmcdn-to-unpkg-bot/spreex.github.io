529e8d2284cd5c22813d083302858e2f342fa870
Denis Gukov <fiftin@outlook.com>
2016-06-22 20:46:39 +0600

    feat: github commit info

--- js/sync.js
+++ js/sync.js
@@ -73,7 +73,7 @@ var Sync = {
     const gh = new GitHub();
     const parts = postOnSite.githubRepoName.split('/');
     return gh.getRepo(parts[0], parts[1]).listCommits().then(function(commits) {
-      var lastCommit = commits[0];
+      var lastCommit = commits[0].data;
       var ret = {
         root: Sync.ROOT,
         path: 'extensions/' + postOnSite.name + '/github',
